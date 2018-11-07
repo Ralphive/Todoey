1649a31c499a87e9cde97f99f3e922481d8e98a6
Ralph <ralph@ralphs-macbook-pro.local>
2018-11-07 18:13:14 +0000

    add tableview delegates and ui checks for select/deselect

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,12 +8,38 @@
 
 import UIKit
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: UITableViewController  {
+    let itemArray = ["TodoOne", "Todo two", "To do three"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK: Tableview DS methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print("User selected row\(indexPath.row) item \(itemArray[indexPath.row])")
+        tableView.deselectRow(at: indexPath, animated: true)
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+                tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
     }
 
 
